ae84fa4d5c17459b092dae9b85802623391f59d5
Alvaro <alvaro.moya.o@gmail.com>
2018-08-28 19:39:19 -0300

    Primera construcción barra navegación de abajo con texto fijo. Iconos y texto incluidos, tamaño ajustado.

--- app/src/main/kotlin/newandroid/com/kotlinnewapp/MainActivity.kt
+++ app/src/main/kotlin/newandroid/com/kotlinnewapp/MainActivity.kt
@@ -1,12 +1,22 @@
 package newandroid.com.kotlinnewapp
 
 import android.support.v7.app.AppCompatActivity
+import android.support.design.widget.BottomNavigationView
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigation)
+
     }
 }
