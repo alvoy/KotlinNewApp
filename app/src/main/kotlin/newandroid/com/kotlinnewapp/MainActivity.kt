6c520a305cede8e78f21d868e2b4ddbba1cb62f4
Alvaro <alvaro.moya.o@gmail.com>
2018-08-29 01:10:03 -0300

    Toolbar de Programa con iconos de búsqueda y settings. Uso de extensiones sintéticas de kotlin para quitar findviewbyid.

--- app/src/main/kotlin/newandroid/com/kotlinnewapp/MainActivity.kt
+++ app/src/main/kotlin/newandroid/com/kotlinnewapp/MainActivity.kt
@@ -1,22 +1,33 @@
 package newandroid.com.kotlinnewapp
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
-import android.support.design.widget.BottomNavigationView
 import android.os.Bundle
-import android.support.v7.app.ActionBar
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var toolbar: ActionBar
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        toolbar = supportActionBar!!
-        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
-        BottomNavigationViewHelper.disableShiftMode(bottomNavigation)
 
+        super.onCreate(savedInstanceState)
+        setTitle("")
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        toolbar_title.text = getString(R.string.programa)
+        toolbar_title.setTextColor(Color.WHITE)
+
+        //val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+        BottomNavigationViewHelper.disableShiftMode(navigationView)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
